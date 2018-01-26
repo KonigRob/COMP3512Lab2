0757bbb2ec7bcf391f21cc644ca601215e6ad321
Rob Koenig <rkoenig5@my.bcit.ca>
2018-01-26 12:39:00 -0800

    adding a menu

--- Stack/Stack/Stack.hpp
+++ Stack/Stack/Stack.hpp
@@ -11,8 +11,8 @@ public:
 	Stack() : top_of_index(EMPTY) {}
 	inline int getIndex() { return top_of_index + 1; }
 	void push(int);
-	void pop(int);
-	int top(int) const;
+	void pop();
+	int top() const;
 	bool empty() const;
 	bool full() const;
 	void print() const;
--- Stack/Stack/Stacktester.cpp
+++ Stack/Stack/Stacktester.cpp
@@ -14,6 +14,46 @@ int main()
 	uniform_real_distribution <double> distrubition(0, 10);
 
 	int randomArr[11];
+	int TWENTY = 20;
+	const int NEIN = 9;
+	char selection;
+	
+
+	cin >> selection;
+	switch (selection)
+	{
+	case 'pu':
+	case 'PU':
+		int entry;
+		cin >> entry;
+		myStack.push(entry);
+		break;
+	case 'pr':
+	case 'PR': 
+		myStack.print();
+		break;
+	case 'po':
+	case 'PO':
+		myStack.pop();
+		break;
+	case 'top':
+	case 'TOP':
+		myStack.top();
+		break;
+	case 'em':
+	case 'EM':
+
+		myStack.empty();
+		break;
+	case 'fu':
+	case 'FU':
+		myStack.full();
+		break;
+	case 'qu':
+	case 'QU':
+		return 0;
+	default: cout << "\n Invalid selection";
+	}
 
 	cout << "adding items directly to the stack" << endl;
 	for (int i = 0; i < size(randomArr); i++) {
@@ -22,6 +62,18 @@ int main()
 		cout << setw(8) << "Adding: " << randomArr[i] << " to index: " << myStack.getIndex() << endl;
 		myStack.push(randomArr[i]);
 	}
+
+	/*//adding 11 to show that it triggers a edge case
+	for (int i = TWENTY; i > NEIN; i--) {
+		cout << "adding: " << i << " to index: " << myStack.getIndex() << endl;
+		myStack.push(i);
+	}*/
+
+	cout << "removing the top off the stack" << endl;
+	myStack.pop();
+	
+
+
 
 	cout << "printing out Stack" << endl << endl;
 
