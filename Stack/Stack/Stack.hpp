b6127f225f66367738451d2fb6b4a74bb5d79b2c
Rob Koenig <rkoenig5@my.bcit.ca>
2018-01-26 11:16:42 -0800

    Added test stuff, I've also been colaberating with Jesse, who's beside me.

--- Stack/Stack/Stack.hpp
+++ Stack/Stack/Stack.hpp
@@ -9,10 +9,12 @@ class Stack
 
 public:
 	Stack() : top_of_index(EMPTY) {}
+	inline int getIndex() { return top_of_index + 1; }
 	void push(int);
 	void pop(int);
 	int top(int) const;
 	bool empty() const;
 	bool full() const;
 	void print() const;
+	
 };
--- Stack/Stack/Stacktester.cpp
+++ Stack/Stack/Stacktester.cpp
@@ -1,3 +1,5 @@
+#include "iostream"
+
 #include "Stack.hpp"
 
 using namespace std;
@@ -5,7 +7,34 @@ using namespace std;
 int main()
 {
 	Stack myStack;
-	//myStack.push;
+	
+	cout << "adding items directly to the stack" << endl;
+	cout << "adding 11 to index: " << myStack.getIndex() << endl;
+	myStack.push(11);
+	cout << "adding 35 to index: " << myStack.getIndex() << endl;
+	myStack.push(35);
+	cout << "adding 24 to index: " << myStack.getIndex() << endl;
+	myStack.push(24);
+	cout << "adding 56 to index: " << myStack.getIndex() << endl;
+	myStack.push(56);
+	cout << "adding 46 to index: " << myStack.getIndex() << endl;
+	myStack.push(46);
+	cout << "adding 1 to index: " << myStack.getIndex() << endl;
+	myStack.push(1);
+	cout << "adding 98 to index: " << myStack.getIndex() << endl;
+	myStack.push(98);
+	cout << "adding 176 to index: " << myStack.getIndex() << endl;
+	myStack.push(176);
+	cout << "adding 94 to index: " << myStack.getIndex() << endl;
+	myStack.push(94);
+	cout << "adding 52 to index: " << myStack.getIndex() << endl;
+	myStack.push(52);
+
+	cout << "printing out Stack" << endl;
+
+	myStack.print();
+
+	system("pause");
 
 	return 0;
 }
